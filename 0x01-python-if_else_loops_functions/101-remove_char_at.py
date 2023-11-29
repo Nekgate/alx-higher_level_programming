240768722e01e0f7bd9beddda0562e040455d81d
Nekgate <Nekgate@gmail.com>
2023-11-29 08:01:19 -0800

    This fuction match bytecode provided by ALX School

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 # Goodluck N. Timothy <@southboy_tim on SM>
 
 
-# This function creates a copy of the string without the character at position n.
+# This func creates a copy of the string without the character at position n
 def remove_char_at(str, n):
     if n < 0:
         return (str)
