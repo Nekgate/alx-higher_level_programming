e314da67c278c86c7038390ddbd78da101c2a815
Nekgate <Nekgate@gmail.com>
2023-12-03 08:29:54 -0800

    This function prints a matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,6 @@
 #!usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    """
-    This print a matrix of integers to STDOUT
-    """
     for i in range(len(matrix)):
         tendr_len = len(matrix[i])
         for j in range(tendr_len):
