388cc0564632e64dbfbc6f0fc1424444d2c10813
Nekgate <Nekgate@gmail.com>
2023-12-18 06:32:55 -0800

    This function divides 2 integers and prints the result.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    t_num = 0
-    for i in range(t_num, x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            t_num += 1
-        except (ValueError, TypeError):
-            pass
-        print()
-        return t_num
+    cnt = 0
+    try:
+        for i in range(x):
+            value = int(my_list[i])
+            print("{:d}".format(value), end=" ")
+            cnt += 1 
+    except (ValueError, IndexError):
+        pass
+    print()
+    return cnt
