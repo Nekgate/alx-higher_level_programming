9181053bfb96ccaf9e65470acc718bc4c31ce22f
Nekgate <Nekgate@gmail.com>
2023-12-18 10:18:11 -0800

    Function in C that print some basic info about Python lists, Python bytes an Python float objects.

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    the_resultt = 0
+    result = 0
     for i in range(1, 3):
         try:
             if (i > a):
                 raise Exception('Too far')
             else:
-                the_result += a ** b / i
+                result += a ** b / i
         except Exception:
-            the_result = b + a
+            result = b + a
             break
-    return the_result
+    return result
