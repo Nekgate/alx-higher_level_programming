e979d037614f69449668147a1c690d799fa76602
Nekgate <Nekgate@gmail.com>
2024-01-09 10:09:11 -0800

    A class Student that defines a student by: (based on 9-student.py)

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""
-A "class_Student" function container
-"""
+''' a class Student that defines a student
+'''
 
 
 class Student:
-    """Representation of a Student"""
+    '''module class student
+    '''
 
-    def __int__(self, first_name, last_name, age):
-        """Initializes the student"""
+    def __init__(self, first_name, last_name, age):
+        '''method __init__
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """returns a dictionary representation of a Student instance"""
+        '''method to_json
+        '''
         return self.__dict__
