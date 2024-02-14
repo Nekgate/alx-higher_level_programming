9418dbd3970755aaf7cce555f1786eeaef400753
Nekgate <Nekgate@gmail.com>
2024-02-14 14:30:28 +0100

    A script that creates the database hbtn_0d_2 and the user user_0d_2.

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,8 @@
--- This script creates the database hbtn_0d_2 and the user_0d_2.
-
--- creating the databas.
+-- creates a database.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- creating user 
+-- creating user with SELECT privileges only
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
 
 -- granting permissions to user_0d_2 for database hbtn_0d_2
 GRANT SELLECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
