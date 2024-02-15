5b1dd47ba93126dab85814c1dbac0a827a46449c
Nekgate <Nekgate@gmail.com>
2024-02-15 12:23:54 +0100

    A script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,13 +1,13 @@
 -- Creates a database that doesn't overwrite pre-existing versions
-CREATES DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Switches the active database
 USE hbtn_0d_usa;
--- Creates a table with column that is foreign key.
-CREATE TABLE IF NOT EXISTS cities (
+-- Creates a table with a column that is a foreign key
+CREATE TABLE IF NOT EXISTS cities(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     UNIQUE(id),
     FOREIGN KEY(state_id)
-        REFERENCE states(id)
+        REFERENCES states(id)
 );
