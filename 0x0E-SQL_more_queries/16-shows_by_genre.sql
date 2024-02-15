1703102c2a856c46afc4a650e735100f6a36fb20
Nekgate <Nekgate@gmail.com>
2024-02-15 12:18:48 +0100

    A script that lists all shows from hbtn_0d_tvshows_rate by their rating.

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,11 +1,10 @@
--- This script lists all Comedy shows in the database hbtn_0d_tvshows.
-
 -- Use of inner join, to join more than 2 tables
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres
-ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title;
+
+-- Prints and joins records from two tables with a matching field
+SELECT tv_shows.title, tv_genres.name
+    FROM tv_shows
+    LEFT JOIN tv_show_genres
+        ON tv_shows.id = tv_show_genres.show_id
+    LEFT JOIN tv_genres
+        ON tv_genres.id = tv_show_genres.genre_id
+    ORDER BY tv_shows.title, tv_genres.name ASC;
