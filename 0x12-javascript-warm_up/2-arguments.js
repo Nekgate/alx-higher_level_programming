7909bb63aedef68749942e9c262c93b972a8ade0
Nekgate <Nekgate@gmail.com>
2024-03-11 19:12:33 +0100

    Script that prints messages depending of the argument.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-const count = process.argv.length;
-
-// Script that prints messages depending of the argument.
-
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Arguments found' : 'Arguments found');
+if (process.argv.length > 3) {
+	console.log('Arguments found');
+} else if (process.argv.length === 3) {
+	console.log('Arguments found');
+} else {
+	console.log('No arguments');
+}
