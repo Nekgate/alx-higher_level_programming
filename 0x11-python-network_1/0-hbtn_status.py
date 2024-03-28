35ec769c1370038e2be6ade9c20c1e906cab9ba1
Nekgate <Nekgate@gmail.com>
2024-03-28 14:55:44 +0100

    A Python script that fetches https://alx-intranet.hbtn.io/status.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@
 
 if __name__ == '__main__':
     import urllib.request
-    url = 'htps://alx-intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print('Body response:')
